09fb62d72a9d7ea18eb2182a818dafe678d290bd
Panagiotis Kalfoudis <panagiotis.kalfoudis@siemens.com>
2025-01-10 16:42:07 +0100

    show 4 smiley faces on stretched edges

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -10,8 +10,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-    vec2 flippedTexCoord = vec2(1.0 - TexCoord.x, TexCoord.y);
-    vec4 color1 = texture(texture1, TexCoord);
-    vec4 color2 = texture(texture2, flippedTexCoord);
-    FragColor = mix(color1, color2, 0.2);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
